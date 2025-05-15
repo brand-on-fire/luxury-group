80e710fbc812c53b16db932d97400ad28eeab72e
brand-on-fire <info@brandonfire.com>
2025-05-15 09:20:39 -0700

    final link rewrites

---
+++ luxurygroup.com/js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($){
+	$('#slider').bjqs({
+		width : 620,
+		height : 310,
+	})
+	
+
+
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var hotelsearch = new Typed('#hotelsearch', {
+    strings: ['City or Country '],
+    attr: 'placeholder',
+    typeSpeed: 80,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true,
+  });
+});
